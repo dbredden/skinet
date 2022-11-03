9e43ec8fc3d276c9d7229caa3b9d00bb52c70707
Danny Redden <dannyredden18@gmail.com>
2022-11-02 22:36:47 -0400

    products controller edits

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,10 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
